4d557b8c0c70278229e8561068ba7ff7bbab3d2d
Raymond Lingxiao <raymondlx@foxmail.com>
2017-07-25 16:50:38 +0800

    Load Json File with Limited Number

---
+++ JsonLoad.py
@@ -0,0 +1,53 @@
+# encoding=utf-8
+import json
+# by doing this, could write in Chinese words
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import os
+import jieba
+
+# Loading all the files under the same path
+def LoadMulti(path,max):
+
+    pathDir = os.listdir(path)
+    for doc in pathDir:
+        LoadSingle(path,doc,max)
+
+def LoadSingle(path,doc,max):
+
+    count = 0
+    with open(path+'/'+doc,'r') as jsonFile:
+        # each line is a object for json
+        for line in jsonFile.readlines():
+            data = json.loads(line)
+            if count<max :
+                SegWord(data["Content"],path,doc,max)
+                count += 1
+
+            # print data["Content"]
+
+def SegWord(sentence,path,doc,max):
+
+    seg_list = jieba.cut(sentence)
+    list =  str("\t".join(seg_list))
+    newPath = os.path.join('./','Corpus')
+    if not os.path.isdir(newPath):
+        os.mkdir(newPath)
+    with open('./Corpus/'+doc.strip('.json')+'_'+str(max),'a') as OutputFile:
+         OutputFile.write(list+'\n')
+
+    # seg_list = jieba.cut("我来到北京清华大学", cut_all=True)
+    # print "Full Mode:", "\t".join(seg_list)  # 全模式
+    # seg_list = jieba.cut("我来到北京清华大学", cut_all=False)
+    # print "Default Mode:", "/ ".join(seg_list)  # 精确模式
+    # seg_list = jieba.cut("他来到了网易杭研大厦")  # 默认是精确模式
+    # print ", ".join(seg_list)
+    # seg_list = jieba.cut_for_search("小明硕士毕业于中国科学院计算所，后在日本京都大学深造")  # 搜索引擎模式
+    # print ", ".join(seg_list)
+
+if __name__ == "__main__":
+    path = './qqnews_ent'
+    max = 2000
+    LoadMulti(path,max)
+    # SegWord()
